beea82d37aaa0cb1ca27e1554d47bf404fddb92a
Sergho <sergche04@gmail.com>
2024-09-17 16:39:49 +0300

    fix: remove mode

--- ega1/constants/constants.ts
+++ ega1/constants/constants.ts
@@ -2,5 +2,4 @@ export const L = 15;
 export const N = 30;
 export const RANDOM_MIN = 1;
 export const RANDOM_MAX = 100;
-export const MODE: number = 1;
 export const SEED = 123;
--- ega1/index.ts
+++ ega1/index.ts
@@ -1,18 +1,11 @@
 import Code from './codes/Code';
-import RandomCode from './codes/RandomCode';
-import { MODE, N, SEED } from './constants/constants';
-import random from 'random';
-import { generateCode } from './util/generateCode';
-
-if(MODE === 1){
-  if(SEED) random.use(SEED);
-  RandomCode.initMap();
-}
+import { N } from './constants/constants';
+import SquareCode from './codes/SquareCode';
 
 let max = new Code();
 for (let i = 0; i < N; i++) {
   let maxChanged = false;
-  const code = generateCode(MODE);
+  const code = new SquareCode();
   code.generate();
 
   if (code.getFitness() > max.getFitness()) {
@@ -21,7 +14,9 @@ for (let i = 0; i < N; i++) {
   }
 
   console.log(
-    `#${i}\t Current: ${code.getPreview()},\t Max: ${max.getPreview()}\t${maxChanged ? '(Max changed)' : ''}`
+    `#${i}\t Current: ${code.getPreview()},\t Max: ${max.getPreview()}\t${
+      maxChanged ? '(Max changed)' : ''
+    }`
   );
 }
 
