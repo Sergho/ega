b217e5aa252b4114a725a0ff54aba7f40f4e4936
Sergho <sergche04@gmail.com>
2024-09-19 13:53:31 +0300

    feat: ega2 main code

--- ega2/codes/Code.ts
+++ ega2/codes/Code.ts
@@ -1,10 +1,12 @@
 import { L } from '../constants/constants';
 import { randomInt } from '../util/randomInt';
+import NaturalCode from './NaturalCode';
 
 class Code {
   public code: number;
 
   constructor(code?: number) {
+    if (code && code > Math.pow(2, L)) throw new Error('Incorrect code');
     this.code = code;
   }
 
@@ -23,6 +25,19 @@ class Code {
 
   getFitness(): number {
     return 0;
+  }
+
+  getClose<T extends Code>(type: { new (code?: number): T }): T[] {
+    const close: T[] = [];
+    const binary = this.getBinary();
+    for (let i = 0; i < L; i++) {
+      let pushed;
+      if (binary[i] === '1')
+        pushed = new type(this.code - Math.pow(2, L - i - 1));
+      else pushed = new type(this.code + Math.pow(2, L - i - 1));
+      close.push(pushed);
+    }
+    return close;
   }
 }
 
---
+++ ega2/index.ts
@@ -0,0 +1,27 @@
+import NaturalCode from './codes/NaturalCode';
+import { N } from './constants/constants';
+import { popRandom } from './util/popRandom';
+
+let max = new NaturalCode();
+max.generate();
+let close: NaturalCode[] = max.getClose(NaturalCode);
+for (let i = 0; i < N; i++) {
+  let maxChanged = false;
+  if (close.length === 0) break;
+
+  const code = popRandom({ list: close });
+
+  if (code.getFitness() > max.getFitness()) {
+    max = code;
+    close = max.getClose(NaturalCode);
+    maxChanged = true;
+  }
+
+  console.log(
+    `#${i}\tCurrent: ${code.getPreview()},\tMax: ${max.getPreview()}\t${
+      maxChanged ? '(Max changed)' : ''
+    }`
+  );
+}
+
+console.log('Result:', max.getPreview());
