aab043d04696826b0d34b4959d8818ba784cf362
Sergho <sergche04@gmail.com>
2024-10-10 22:48:31 +0300

    feat: add print landscape method to ega1

---
+++ ega1/util/printLandscape.ts
@@ -0,0 +1,13 @@
+import SquareCode from '../codes/SquareCode';
+
+export class PrintLandscapeOptions {
+  limit: number;
+}
+
+export const printLandscape = (options: PrintLandscapeOptions): void => {
+  console.log('Landscape:');
+  for (let i = 0; i < options.limit; i++) {
+    const code = new SquareCode(i);
+    console.log(code.getPreview());
+  }
+};
