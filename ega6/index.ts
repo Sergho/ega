2c67fafd936518d796b89174f4b5af242ab3de6f
Sergho <sergche04@gmail.com>
2024-12-16 08:44:05 +0300

    fix: ega6 output

--- ega6/index.ts
+++ ega6/index.ts
@@ -3,6 +3,12 @@ import { FileReader } from './src/file-reader/file-reader';
 
 const reader = new FileReader(INPUT_FILENAME);
 const tree = reader.buildTree();
+
+console.log('Tree:');
 console.log(tree.info());
-console.log(tree.optimal());
-console.log(tree.optimum());
+if (tree.isCompatible()) {
+  console.log(tree.optimal());
+  console.log(tree.optimum());
+} else {
+  console.log('Incompatible data!');
+}
